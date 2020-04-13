04d0b895f15243cb87d3ee7ee42a22d9c0335f01
Nivaceb <63502621+Nivaceb@users.noreply.github.com>
2020-04-13 15:16:01 +0200

    Unity project
    test project

---
+++ New Unity Project/Assets/Test/Machin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Machin : MonoBehaviour
+{
+    public GameObject Roger;
+    private Vector3 objectPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Roger.transform.position = new Vector3(90, 90, 90);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("salut");
+        objectPosition = Roger.transform.position;
+        Debug.Log(objectPosition);
+        objectPosition.x += 0.1f;
+        Roger.transform.position = objectPosition; 
+        
+    }
+}
